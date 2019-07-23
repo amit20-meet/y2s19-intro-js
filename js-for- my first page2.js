6fd61885ce1e655a880f91d476e88e94c2ec0b2a
myname21-meet <myname21@meet.mit.edu>
2019-07-23 18:30:20 +0300

    little of Js

---
+++ js-for- my first page2.js
@@ -0,0 +1,3 @@
+alert("this is a page for lover's dogs only' if u love cats go away");
+var name1 prompt("now i know u are a realy dog lover. so my bro what's your name?");
+console.log("hello "+ name1);
