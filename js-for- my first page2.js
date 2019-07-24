6b12078c140c41cff661727dda9749a190afd6e0
myname21-meet <myname21@meet.mit.edu>
2019-07-24 18:32:32 +0300

    hi there is so challenge here

--- js-for- my first page2.js
+++ js-for- my first page2.js
@@ -5,8 +5,8 @@ if (lover =="yes") {
 	var name1= prompt("now i know u are a realy dog lover. so my bro what's your name?");
 	alert("hello "+ name1);
 } else{
-	alert("go awa"y);
-	while(lover==no){
+	alert("go away");
+	while(lover!="yes"){
 		var lover = prompt("are u a lover dogs?[answer in yes/no");
 
 		if (lover =="yes") {
